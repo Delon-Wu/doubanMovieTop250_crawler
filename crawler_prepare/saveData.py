812eb71ce634c0ec1940e9eb90822cb657a87c56
wujinlong <wjl@posun.cn>
2021-06-25 18:31:43 +0800

    fun: 导出xml改为导出db数据库

--- crawler_prepare/saveData.py
+++ crawler_prepare/saveData.py
@@ -6,9 +6,23 @@
 
 # 生成exel表的
 import xlwt
+import sqlite3
 
 
 def saveData(data_list,save_path="豆瓣电影Top250.xls"):
+    connection = sqlite3.connect('doubanMoviesTop250.db')
+    cursor = connection.cursor()
+    try:
+        cursor.execute('''CREATE TABLE doubanMovie 
+        (detail_link text, cover_url text, movie_name text(50), movie_name_foreign text(50), rating text(5), judge_num text(20), comment text(300), movie_about text(200), id integer PRIMARY KEY AUTOINCREMENT)
+        ''')
+    except sqlite3.OperationalError as e:
+        print(e, '该表可能已存在')
+    for report in data_list:
+        cursor.execute('''INSERT INTO doubanMovie  values(?, ?, ?, ?, ?, ?, ?, ?, ?)''', tuple(report))
+    connection.commit()
+    connection.close()
+    return
     try:
         workbook = xlwt.Workbook('utf-8')
         worksheet = workbook.add_sheet("sheet1")
